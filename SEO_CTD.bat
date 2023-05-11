c5dd31d102659067d4a849efbb04cc4f9112f9f8
Jeff Bowman <jsbowman@ucsd.edu>
2023-05-11 15:46:39 -0700

    Update SEO_CTD.bat

--- SEO_CTD.bat
+++ SEO_CTD.bat
@@ -1,5 +1,5 @@
 :repeat_forever
-start /w "C:\Program Files\Sea-Bird\SeasaveV7\seasave.exe" /autostart=SEO_CTD.psa /u /autostop=1440
+start /w "C:\Program Files\Sea-Bird\SeasaveV7\seasave.exe /autostart=SEO_CTD.psa /u /autostop=1440"
 goto repeat_forever
 
 
