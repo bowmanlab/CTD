fbf18b7777211e274c765b091e58134cd24a7e39
Jeff Bowman <jsbowman@ucsd.edu>
2023-05-13 17:11:11 -0700

    Added command for DatCnvW.exe

--- SEO_CTD.bat
+++ SEO_CTD.bat
@@ -1,6 +1,16 @@
-cd C:\Program Files (x86)\Sea-Bird\SeasaveV7
+::Must change paths
+::Modify SEO_CTD_DatCnv.psa so that it calls instrument calibration file
+::Check time on CTD! Probably set all instruments and computer to UTC at this point.
+
 :repeat_forever
-start /w Seasave.exe /autostart=C:\Users\bowman-lab\Desktop\CTD\SEO_CTD.psa /u /autostop=1440
+
+::Operate CTD for 15 minutes with default naming scheme.
+::This will execute in a separate terminal and the script will wait to proceed until execution is complete.
+start /w /D "C:\Program Files (x86)\Sea-Bird\SeasaveV7" Seasave.exe /autostart=C:\Users\bowman-lab\Desktop\CTD\SEO_CTD.psa /u /autostop=15
+
+::Convert any .hex format files according to the calibration data contained in the instrument calibration file, pointed to from the *DatCnv.psa file.
+::This will execute in a separate terminal and the script will wait to proceed until execution is complete.
+start /w /D "C:\Program Files (x86)\Sea-Bird\SBEDataProcessing-Win32" DatCnvW.exe /pC:\Users\jeff\Documents\bowman_lab\bowman_lab_github\CTD\SEO_CTD_DatCnv.psa /iC:\Users\jeff\Downloads\*.hex /s
 goto repeat_forever
 
 
