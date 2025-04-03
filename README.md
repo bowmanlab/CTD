45a11b9c0a5c866b52204b446fd4b584d2f4271f
Jeff Bowman <jsbowman@ucsd.edu>
2025-04-03 14:20:52 -0700

    new calibration

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 # CTD
-## File dependencies
-Both .psa files expect the calibration settings at C:\Users\bowman-lab\Desktop\2023-05-03_CTD_calibration_settings.xmlcon
-
-## Executable dependencies
-C:\Program Files (x86)\Sea-Bird\SeasaveV7\Seasave.exe
-C:\Program Files (x86)\Sea-Bird\SBEDataProcessing-Win32\DatCnvW.exe
-
-## PSA files
-SEO_CTD.psa controls how the data is collected and written. Data file path is determined by <DataFilePath> even when /u flag is used in command line.
-
-SEO_CTD_DatCnv.psa controls how the hex files are converted to cnv files. The write location for the cnv files is determined by <OutputDir>.
--- SEO_CTD.bat
+++ SEO_CTD.bat
@@ -1,4 +1,5 @@
 ::This batch file executes the commands for continuously running the CTD and converting the hex files to cnv.
+::You must start logging on the CTD in seaterm with the "startnow" command and appropriate sampling interval before executing this script.
 
 :repeat_forever
 
@@ -8,11 +9,11 @@ start /w /D "C:\Program Files (x86)\Sea-Bird\SeasaveV7" Seasave.exe /autostart=C
 
 ::Convert any .hex format files according to the calibration data contained in the instrument calibration file, pointed to from the *DatCnv.psa file.
 ::This will execute in a separate terminal and the script will wait to proceed until execution is complete. Files are written to the location specified in the psa file.
-start /w /D "C:\Program Files (x86)\Sea-Bird\SBEDataProcessing-Win32" DatCnvW.exe /pC:\Users\bowman-lab\Desktop\CTD\SEO_CTD_DatCnv.psa /iC:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.hex /s
+start /w /D "C:\Program Files (x86)\Sea-Bird\SBEDataProcessing-Win32" DatCnvW.exe /pC:\Users\bowman-lab\Desktop\CTD\SEO_CTD_DatCnv.psa /iC:\Users\bowman-lab\Desktop\CTD\CTD_Data_temp\*.hex /s
 
-MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.hex C:\Users\bowman-lab\Dropbox\CTD_Data_v1\
-MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.XMLCON C:\Users\bowman-lab\Dropbox\CTD_Data_v1\
-MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.hdr C:\Users\bowman-lab\Dropbox\CTD_Data_v1\
+MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.hex C:\Users\bowman-lab\Dropbox\CTD_Data_v2\
+MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.XMLCON C:\Users\bowman-lab\Dropbox\CTD_Data_v2\
+MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.hdr C:\Users\bowman-lab\Dropbox\CTD_Data_v2\
 
 goto repeat_forever
 
