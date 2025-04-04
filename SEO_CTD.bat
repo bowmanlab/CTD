07fc3d64037073bb80406ecd8ce869ec9065851c
Jeff Bowman <jsbowman@ucsd.edu>
2025-04-04 07:01:13 -0700

    Update SEO_CTD.bat

--- SEO_CTD.bat
+++ SEO_CTD.bat
@@ -11,9 +11,9 @@ start /w /D "C:\Program Files (x86)\Sea-Bird\SeasaveV7" Seasave.exe /autostart=C
 ::This will execute in a separate terminal and the script will wait to proceed until execution is complete. Files are written to the location specified in the psa file.
 start /w /D "C:\Program Files (x86)\Sea-Bird\SBEDataProcessing-Win32" DatCnvW.exe /pC:\Users\bowman-lab\Desktop\CTD\SEO_CTD_DatCnv.psa /iC:\Users\bowman-lab\Desktop\CTD\CTD_Data_temp\*.hex /s
 
-MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.hex C:\Users\bowman-lab\Dropbox\CTD_Data_v2\
-MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.XMLCON C:\Users\bowman-lab\Dropbox\CTD_Data_v2\
-MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_v1\*.hdr C:\Users\bowman-lab\Dropbox\CTD_Data_v2\
+MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_temp\*.hex C:\Users\bowman-lab\Dropbox\CTD_Data_v2\
+MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_temp\*.XMLCON C:\Users\bowman-lab\Dropbox\CTD_Data_v2\
+MOVE C:\Users\bowman-lab\Desktop\CTD\CTD_Data_temp\*.hdr C:\Users\bowman-lab\Dropbox\CTD_Data_v2\
 
 goto repeat_forever
 
